cd87ea95169660695f4be176f8b5f3af5c8455ae
Tanvir Tasin <ttasin2x@gmail.com>
2026-05-04 19:29:01 +0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'unijoy-v1';
+const assets = ['./', './index.html', './photos/logo.webp'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
